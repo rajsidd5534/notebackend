d836a47f62d9808fc845c3ede558c1f13392f6c5
Md Raj Siddiqui <rajsidd5534@gmail.com>
2025-09-04 17:29:01 +0530

    changes

--- src/main/java/com/noteapp/noteaap/config/CorsConfig.java
+++ src/main/java/com/noteapp/noteaap/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.noteapp.noteaap.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,17 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    // Use environment variable for frontend URL
-    @Value("${FRONTEND_URL}")
-    private String frontendUrl;
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(frontendUrl)  // Vercel link here
+                        .allowedOrigins(
+                                "https://notefrontent-io77az22r-my-projectteam.vercel.app",
+                                "https://notefrontent-phi.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
